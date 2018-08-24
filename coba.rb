5e292bb8efa9e9df46abfcdd0b06742dfc8a7ce5
Muhamad Syahrul Mubarok <12172949@bsi.ac.id>
2018-08-24 17:31:43 +0000

    automatically backed up by learn

--- coba.rb
+++ coba.rb
@@ -1,8 +1,7 @@
-3.times { print "Hello!" }
-# > Hello!Hello!Hello!
+def restaurant
+  restaurant_name = "Guy's American Kitchen & Bar"
+  cuisine = "american"
+  motto = "Welcome to Flavor Town!"
+end
 
-3.times { print "Hello!" }
-# > Hello!
-# > Hello!
-# > Hello!
-print " wtf"
+restaurant
